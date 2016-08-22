bc44fd23138480eba09913dd2a39680066b492f0
Luciano Mammino <lucianomammino@gmail.com>
2016-08-22 21:18:14 +0100

    removed "this" in component render

--- src/components/AthletePage.js
+++ src/components/AthletePage.js
@@ -12,35 +12,35 @@ const athletes = require('./../../app/athletes');
 class AthletePage extends React.Component {
   render() {
     const id = this.props.params.id;
-    this.athlete = athletes.filter((athlete) => athlete.id === id)[0];
-    if (!this.athlete) {
+    const athlete = athletes.filter((athlete) => athlete.id === id)[0];
+    if (!athlete) {
       return <NotFoundPage/>
     }
-    const headerStyle = {backgroundImage:`url(/img/${this.athlete.cover})`};
+    const headerStyle = {backgroundImage:`url(/img/${athlete.cover})`};
     return (
       <Layout>
         <nav className="atheletes-menu">
-          {athletes.map(athlete => {
-            const status = athlete.id === this.athlete.id ? 'active' : null;
-            return <Link key={athlete.id} to={`/athlete/${athlete.id}`} className={status}>
-              {athlete.name}
+          {athletes.map(menuAthlete => {
+            const status = menuAthlete.id === athlete.id ? 'active' : null;
+            return <Link key={menuAthlete.id} to={`/athlete/${menuAthlete.id}`} className={status}>
+              {menuAthlete.name}
             </Link>
           })}
         </nav>
         <div className="athlete">
           <header style={headerStyle}/>
           <div className="picture-container">
-            <img src={`/img/${this.athlete.image}`}/>
-            <h2 className="name"><Flag code={this.athlete.country}/> {this.athlete.name} ({this.athlete.country})</h2>
+            <img src={`/img/${athlete.image}`}/>
+            <h2 className="name"><Flag code={athlete.country}/> {athlete.name} ({athlete.country})</h2>
           </div>
           <section className="description">
-            Olympic medalist from <strong><Flag code={this.athlete.country} showName="true"/></strong>,  
-            born in {this.athlete.birth} (Find out more on <a href={this.athlete.link} target="_blank">Wikipedia</a>).
+            Olympic medalist from <strong><Flag code={athlete.country} showName="true"/></strong>,  
+            born in {athlete.birth} (Find out more on <a href={athlete.link} target="_blank">Wikipedia</a>).
           </section>
           <section className="medals">
-            <p>Winner of <strong>{this.athlete.medals.length}</strong> medals:</p>
+            <p>Winner of <strong>{athlete.medals.length}</strong> medals:</p>
             <ul>{
-              this.athlete.medals.map((medal, i) => <Medal key={i} {...medal}/>)
+              athlete.medals.map((medal, i) => <Medal key={i} {...medal}/>)
             }</ul>
           </section>
         </div>
