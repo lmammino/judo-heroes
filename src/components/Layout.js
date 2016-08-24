3e8f6e1f2930b92b05df6b30f1effc6ecac38e5c
Luciano Mammino <lucianomammino@gmail.com>
2016-08-25 00:00:17 +0100

    Updated meta title and footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,7 +15,7 @@ export default class Layout extends React.Component {
         <div className="app-content">{this.props.children}</div>
         <footer>
           <p>
-            This is a simple demo application to showcase how to achieve universal rendering with <strong>React</strong> and <strong>Express</strong>.
+            This is a demo app to showcase universal rendering and routing with <strong>React</strong> and <strong>Express</strong>.
           </p>
 
           <p>
