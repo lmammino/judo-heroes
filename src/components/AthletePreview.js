23858f4f11491196a260a3e6df5d49e00be9e2af
Luciano Mammino <lucianomammino@gmail.com>
2016-08-26 01:24:06 +0100

    replaced medal emojii with icon

--- src/components/AthletePreview.js
+++ src/components/AthletePreview.js
@@ -10,7 +10,7 @@ export default class AthletePreview extends React.Component {
         <div className="athlete-preview">
           <img src={`img/${this.props.image}`}/>
           <h2 className="name">{this.props.name}</h2>
-          <span className="medals-count">🏅 {this.props.medals.length}</span>
+          <span className="medals-count"><img src="/img/medal.png"/> {this.props.medals.length}</span>
         </div>
       </Link>
     );
