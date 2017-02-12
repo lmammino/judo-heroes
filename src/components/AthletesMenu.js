c9c7e1fc06dfe6ae24857df8082efc688ea40ccc
Seifeddine Jerbi <marwen109@gmail.com>
2017-02-12 23:17:07 +0100

    Passed athletes params using props instead of duplicated import

--- src/components/AthletesMenu.js
+++ src/components/AthletesMenu.js
@@ -2,13 +2,12 @@
 
 import React from 'react';
 import { Link } from 'react-router';
-import athletes from '../data/athletes';
 
 export default class AthletesMenu extends React.Component {
   render() {
     return (
       <nav className="athletes-menu">
-        {athletes.map(menuAthlete => {
+        {this.props.athletes.map(menuAthlete => {
           return <Link key={menuAthlete.id} to={`/athlete/${menuAthlete.id}`} activeClassName="active">
             {menuAthlete.name}
           </Link>;
